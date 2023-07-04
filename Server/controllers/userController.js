83529e162bc07eef080371cbbbdc76ce3a9d4a71
kc-code32 <kelvinchen138@gmail.com>
2023-07-04 12:34:04 -0500

    created login, signup, and authentication functionality and related models

---
+++ Server/controllers/userController.js
@@ -0,0 +1,119 @@
+const axios = require('axios')
+const User = require('../models/UserModel');
+const bcrypt = require('bcryptjs');
+
+// init const userController, an object that stores the functionality 
+const userController = {};
+
+// need to pass above object and the API key into the request to the API. 
+// we added some .env stuff, not being used as this page is sufficient
+    // having a .env folder for private info like an API key or database log in info is best practice
+    // but we didn't have time to implement/didn't need to because of the scope of this project
+
+
+// The getStretches method is a function that accepts 3 params, req, res, next, and stores the result of a fetch request to the exercises api in our 
+userController.getStretches = async (req, res, next) => {
+    try { 
+        console.log('HIT!!!')
+        // init const muscle as muscle prop of request query
+        const { muscle } = req.body; 
+        console.log(muscle)
+        // init const apiRes as output from api request
+        const response = await axios.get(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}&type=stretching`, {
+            headers: { 'x-api-key': 'SReYt5aEyGMKzrdSe87wew==boZAObqiLCiQPGrb'}
+        });
+
+        console.log(response.data)
+
+        res.locals.apiRes = response.data;
+        return next();
+    } catch (error) {
+        const errorObject = {
+            // log to developer
+            log: 'Error occurred in StretchController.GetExercise',
+            // message to client
+            message: { error: 'An error has occurred in getting an exericse'},
+            status: 400
+        };
+        // pass error object to global error handler
+        return next(errorObject);
+    }
+}
+
+
+userController.createUser = (req, res, next) => {
+  const { email, name, password } = req.body;
+  if (!email || !password || !name) {
+    res.locals.signedIn = false;
+    console.log('fail to create user, missing register info')
+    return next();
+  }
+
+  User.create({ email, name, password }, (err, user) => {
+    if (err) {
+      res.locals.signedIn = false;
+      //   return next();
+      return next({
+        log: 'Error occurred in userController.createUser',
+        status: 500,
+        message: { err: 'An error occurred in userController.createUser'}
+      });
+    } else {
+      res.locals.userDetail = user;
+      res.locals.userId = user.id;
+      res.locals.signedIn = true;
+      return next();
+    }
+  });
+};
+
+/**
+* verifyUser - Obtain username and password from the request body, locate
+* the appropriate user in the database, and then authenticate the submitted password
+* against the password stored in the database.
+*/
+userController.verifyUser = (req, res, next) => {
+  // write code here
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.locals.signedIn = false;
+    return next();
+  }
+
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      res.locals.signedIn = false;
+    //   return next();
+      return next({
+        log: 'Error occurred in userController.verifyUser',
+        status: 500,
+        message: { err: 'An error occurred in userController.verifyUser'}
+      });
+    } else {
+      bcrypt.compare(password, user.password)
+        .then((result) => {
+          if (!result) {
+            res.locals.signedIn = false;
+            console.log('user entered incorrect password')
+            return next();
+          } else {
+            res.locals.signedIn = true;
+            res.locals.userDetail = user;
+            res.locals.userId = user.id;
+            return next();
+          }
+        })
+        .catch((err) => {
+          res.locals.signedIn = false;
+        //   return next();
+          return next({
+            log: 'Error occurred in userController.verifyUser',
+            status: 500,
+            message: { err: 'An error occurred in userController.verifyUser'}
+          });
+        });
+    }
+  });
+};
+    
+module.exports = userController;
---
+++ Server/routes/serverRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
+const sessionController = require('../controllers/sessionController');
+
+// responses to different requests from front-end
+
+router.post(
+  '/', 
+  userController.getStretches, 
+  (req, res) => {
+    return res.status(200).json(res.locals.apiRes);
+});
+
+router.post(
+  '/signup',
+  userController.createUser,
+  cookieController.setSSIDCookie,
+  sessionController.startSession,
+  (req, res) => {
+    return res.status(200).json({  
+      loggedIn: res.locals.signedIn,
+      userDetail: res.locals.userDetail,
+    });
+  }
+);
+
+router.post(
+  '/login',
+  userController.verifyUser,
+  cookieController.setSSIDCookie,
+  sessionController.startSession,
+  (req, res) => {
+    return res.status(200).json({ 
+      loggedIn: res.locals.signedIn,
+      userDetail: res.locals.userDetail,
+    });
+  }
+);
+
+router.get(
+  '/isLoggedIn', 
+  sessionController.isLoggedIn, 
+  (req, res) => {
+    return res.json({ 
+      loggedIn: res.locals.signedIn, 
+      id: req.cookies.ssid });
+  }
+);
+
+router.get(
+  '/logout', 
+  (req, res) => {
+  return res.status(200).clearCookie('cookieId').redirect('/');
+  }
+);
+
+module.exports = router;
+
+

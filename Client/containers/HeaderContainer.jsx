a29112d261467702b670bb4c799390838e853ee1
Brok3Turtl3 <brok3turtl3@gmail.com>
2023-07-06 08:28:32 -0400

    Nav updates

--- Client/containers/HeaderContainer.jsx
+++ Client/containers/HeaderContainer.jsx
@@ -1,59 +1,83 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../styles.css';
 import RegisterModal from '../components/RegisterModal.jsx';
 import LoginModal from '../components/LoginModal.jsx';
 import { logout } from '../actions/userActions';
 
-
 const HeaderContainer = () => {
-  const userInfo = useSelector((state) => state.userLogin);
-
+  const location = useLocation();
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state.userLogin);
+  const currentPath = location.pathname;
 
   const handleLogout = () => {
-    console.log('logout hit!!!')
-    dispatch(logout())
-    navigate('/')
-  }
+    console.log('logout hit!!!');
+    dispatch(logout());
+    navigate('/');
+  };
 
   const handleSearch = () => {
-    navigate('/search')
-  }
+    navigate('/search');
+  };
+
+  const handleHomePage = () => {
+    navigate('/homepage');
+  };
 
   return (
     <div id='navBar'>
       <div id='flex-item'>
         <p>Stretch.io</p>
       </div>
-      
 
-      {userInfo?.userInfo ? (
-        <div id='flex-item' className='authButtons'>
-          <div className='authButton'>
-            <button className='submitButton' onClick={handleSearch}>Search Stretches</button>
-          </div>
-          <div className='authButton'>
-            <button className='submitButton' onClick={handleLogout}>Logout</button>
-          </div>
-        </div>
-      ) : (
-        <div id='flex-item' className='authButtons'>
-          <div className='authButton'>
-            <RegisterModal />
-          </div>
-          <div className='authButton'>
-            <LoginModal />
-          </div>
-        </div>
-      )}
+      <div id='flex-item' className='authButtons'>
+        {userInfo?.userInfo && (
+          <>
+            {currentPath === '/search' && (
+              <div className='authButton active'>
+                <button className='submitButton' onClick={handleHomePage}>
+                  HomePage
+                </button>
+              </div>
+            )}
+
+            {currentPath === '/homepage' && (
+              <div className='authButton active'>
+                <button className='submitButton' onClick={handleSearch}>
+                  Search Stretches
+                </button>
+              </div>
+            )}
+
+            <div className='authButton'>
+              <button className='submitButton' onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
+          </>
+        )}
+
+        {!userInfo?.userInfo && (
+          <>
+            <div className='authButton'>
+              <RegisterModal />
+            </div>
+            <div className='authButton'>
+              <LoginModal />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
 export default HeaderContainer;
+
+
 
 
 
